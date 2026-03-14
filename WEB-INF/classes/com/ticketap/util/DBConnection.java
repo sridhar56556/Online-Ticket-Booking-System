80979c8bb0f60926da1e6832653616bc3d6a689b
Sridhr <sridharkonda553@gmail.com>
2026-03-14 17:27:47 +0530

    update database connection to online MySQL

--- WEB-INF/classes/com/ticketap/util/DBConnection.java
+++ WEB-INF/classes/com/ticketap/util/DBConnection.java
@@ -6,18 +6,16 @@ import java.sql.DriverManager;
 public class DBConnection {
 
     public static Connection getConnection() {
+
         Connection con = null;
 
         try {
-            // Load MySQL Driver
+
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // DB details (change password if needed)
-           Connection con = DriverManager.getConnection(
-"jdbc:mysql://sql12.freesqldatabase.com:3306/sql12819964",
-"sql12819964",
-"1Dt4GyslnB"
-); // <-- change this
+            String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12819964";
+            String user = "sql12819964";
+            String password = "1Dt4GyslnB";
 
             con = DriverManager.getConnection(url, user, password);
 
